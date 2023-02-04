fa01e5efa2a330fd38b2696d1dc8c04c3efd54eb
sandynguyen8928 <sandynguyen8928@gmail.com>
2023-02-04 14:43:49 -0500

    added landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:8000/message")
-        .then((res) => res.json())
-        .then((data) => setMessage(data.message));
-  }, []);
+  // const [message, setMessage] = useState("");
+  //
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/message")
+  //       .then((res) => res.json())
+  //       .then((data) => setMessage(data.message));
+  // }, []);
 
   return (
-      <div className="App">
-        <h1>{message}</h1>
+      <div>
+        <LandingPage />
       </div>
   );
 }
